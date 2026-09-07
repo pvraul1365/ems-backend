a1802518519017867fc376ff62c44579c52d7399
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-09-07 16:12:05 +0200

    347. Create Employee JPA Entity

---
+++ src/main/java/net/javaguides/ems/entity/Employee.java
@@ -0,0 +1,43 @@
+package net.javaguides.ems.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Employee
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - raul.perez.vicente@gmail.com
+ * @version 07/09/2026 - 16:05
+ * @since 1.25
+ */
+@Entity
+@Table(name = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+}
